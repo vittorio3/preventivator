bd9bc1fa4401f4c1f2b296db6bd09a6d0145eaae
vittorio3 <121828660+vittorio3@users.noreply.github.com>
2026-06-14 12:31:02 +0200

    lavori del 14 giugno

---
+++ preventivator/dbo/Tables/MULTIFUNCTION.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[MULTIFUNCTION](
+  [model] VARCHAR(25) PRIMARY KEY, -- Name for example ET2950
+  [make] VARCHAR (25) NOT NULL, -- Make for example EPSON
+  [type] VARCHAR(6) NOT NULL, -- It can be laser or inkjet
+)
