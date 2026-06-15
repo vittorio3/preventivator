dbe47161bb56c91d2275b59d941a2dd62be3d3e3
pcLavoro <stirparovittorio9@gmail.com>
2026-06-15 18:04:07 +0200

    NodeJs

--- preventivator/dbo/Tables/MULTIFUNCTION.sql
+++ preventivator/dbo/Tables/MULTIFUNCTION.sql
@@ -2,4 +2,5 @@ CREATE TABLE [dbo].[MULTIFUNCTION](
   [model] VARCHAR(25) PRIMARY KEY, -- Name for example ET2950
   [make] VARCHAR (25) NOT NULL, -- Make for example EPSON
   [type] VARCHAR(6) NOT NULL, -- It can be laser or inkjet
+  [image] VARCHAR(50)NOT NULL -- Root of the image
 )
